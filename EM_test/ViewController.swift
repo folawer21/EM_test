a1f41de4fec90e39bbec5938ec7e5d8630752eb7
folawer21 <folawer21@mail.ru>
2025-03-20 19:30:58 +0300

    Initial Commit

---
+++ EM_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EM_test
+//
+//  Created by Александр  Сухинин on 20.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
